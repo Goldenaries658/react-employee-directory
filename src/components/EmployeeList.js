bf636ffd6b076229762180a932f104b63fa705b3
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-17 16:41:02 +0100

    Created basic table

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,6 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Employee from './Employee';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 export default function EmployeeList() {
   const [employees, setEmployees] = useState([]);
@@ -20,18 +34,26 @@ export default function EmployeeList() {
     fetchEmployees();
   }, []);
 
+  const classes = useStyles();
+
   return (
-    <ul>
-      {employees.map(({ id, name, username, email, phone, website }) => (
-        <Employee
-          key={id}
-          name={name}
-          username={username}
-          email={email}
-          phone={phone}
-          website={website}
-        />
-      ))}
-    </ul>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Username</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Phone</TableCell>
+            <TableCell align="right">Website</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {employees.map((employee) => (
+            <Employee key={employee.id} employee={employee} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
