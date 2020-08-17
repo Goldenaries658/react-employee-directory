50a038c708a048d67bd7f47773131d40d80c0912
Goldenaries658 <Goldenaries658@users.noreply.github.com>
2020-08-17 16:44:41 +0000

    Prettified Code!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { Container } from "@material-ui/core/";
-import EmployeeTable from "./components/EmployeeTable";
-import AppBar from "./components/AppBar";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Container } from '@material-ui/core/';
+import EmployeeTable from './components/EmployeeTable';
+import AppBar from './components/AppBar';
 
 export default function App() {
   return (
