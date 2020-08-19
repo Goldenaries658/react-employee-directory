27eec099f05d16bedde2d4f4529cc9b2537ed8e3
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-19 03:12:46 +0100

    Header render test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Employee Directory/i);
   expect(linkElement).toBeInTheDocument();
 });
